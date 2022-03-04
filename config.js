a96ee1ffcca0857392760a6bf6e8de1d6c39999b
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-04 23:32:31 +0700

    Add files via upload
    kaka

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100008584600967&sk=photos_by';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng Phúc';
+const contentCard = 'Chúc cho Phúc có một ngày 8/3 vui vẻ và hạnh phúc. Chúc Phúc làm được những điều mình muốn và đạt được nhiều thành công trong tương lai. À, đừng dọa bồng Lợi đôi sông Hàn nữa nhé!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'phuc.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
